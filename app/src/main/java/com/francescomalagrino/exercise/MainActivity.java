18ddbba643a6aa42030c82a62639b9dcb3de7240
Francesco Malagrino <francesco.malagrino@gmail.com>
2021-11-28 18:22:49 +0100

    Exercise done
    Exercise done

---
+++ app/src/main/java/com/francescomalagrino/exercise/MainActivity.java
@@ -0,0 +1,14 @@
+package com.francescomalagrino.exercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
